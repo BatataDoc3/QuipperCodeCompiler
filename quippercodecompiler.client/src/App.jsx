3b73d1fbad1f0886ff0cada6c3d0e5f66d63034e
Jorge Miguel Silva Melo <ema.m.m.martins@gmail.com>
2025-02-03 21:03:49 +0000

    Add project files.

---
+++ quippercodecompiler.client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+    return (
+        <div>
+            <h1> Hello World! </h1>
+            <p>This component demonstrates fetching data from the server.</p>
+        </div>
+    );
+    
+
+}
+
+export default App;
